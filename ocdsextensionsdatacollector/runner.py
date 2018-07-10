e40e7bfaa5c49a3ac6b24c39d17a8c07727ee299
James Baster <james.baster@opendataservices.coop>
2018-07-10 09:54:14 +0100

    Break code up into functions for readability and testability

--- ocdsextensionsdatacollector/runner.py
+++ ocdsextensionsdatacollector/runner.py
@@ -14,58 +14,66 @@ class Runner:
         self.extensions_data = 'https://raw.githubusercontent.com/open-contracting/extension_registry/master/extensions.csv' # noqa
         self.extension_versions_data = 'https://raw.githubusercontent.com/open-contracting/extension_registry/master/extension_versions.csv'  # noqa
         self.sample = sample
+        self.out = None
         if not os.path.isdir(self.output_directory):
             os.mkdir(self.output_directory)
 
     def run(self):
-
-        out = {
+        self.out = {
             'extensions': {}
         }
 
         registry = ExtensionRegistry(self.extension_versions_data, self.extensions_data)
 
         for version in registry:
-            if self.sample and len(out['extensions']) >= 5:
+            if self.sample and len(self.out['extensions']) >= 5:
                 continue
 
-            # Part 1 : add  basic info to output
-            if version.id not in out['extensions']:
-                out['extensions'][version.id] = {
-                    'versions': {},
-                    'category': version.category,
-                    'core': version.core
-                }
+            self._add_basic_info_to_output(version)
+            self._download_version(version)
+            self._add_information_from_download_to_output(version)
 
-            out['extensions'][version.id]['versions'][version.version] = {
-                'date': version.date,
-                'base_url': version.base_url,
-                'download_url': version.download_url,
+        self._write_output()
+
+    def _add_basic_info_to_output(self, version):
+        if version.id not in self.out['extensions']:
+            self.out['extensions'][version.id] = {
+                'versions': {},
+                'category': version.category,
+                'core': version.core
             }
 
-            # Part 2: download the extension
-            version_output_dir = os.path.join(self.output_directory, version.id, version.version)
-            os.makedirs(version_output_dir)
+        self.out['extensions'][version.id]['versions'][version.version] = {
+            'date': version.date,
+            'base_url': version.base_url,
+            'download_url': version.download_url,
+        }
 
-            response = requests.get(version.download_url, allow_redirects=True)
-            response.raise_for_status()
-            version_zipfile = zipfile.ZipFile(io.BytesIO(response.content))
-            names = version_zipfile.namelist()
-            start = len(names[0])
-            for name in names[1:]:
-                if name[-1:] == '/':
-                    os.makedirs(os.path.join(version_output_dir, name[start:]))
-                else:
-                    with open(os.path.join(version_output_dir, name[start:]), "wb") as outfile:
-                        outfile.write(version_zipfile.read(name))
+    def _download_version(self, version):
+        version_output_dir = os.path.join(self.output_directory, version.id, version.version)
+        os.makedirs(version_output_dir)
 
-            # Part 3 : add information from extension to output
-            with open(os.path.join(version_output_dir, "extension.json")) as infile:
-                extension_json = json.load(infile)
-                out['extensions'][version.id]['versions'][version.version]['name'] = \
-                    extension_json['name']
-                out['extensions'][version.id]['versions'][version.version]['description'] = \
-                    extension_json['description']
+        response = requests.get(version.download_url, allow_redirects=True)
+        response.raise_for_status()
+        version_zipfile = zipfile.ZipFile(io.BytesIO(response.content))
+        names = version_zipfile.namelist()
+        start = len(names[0])
+        for name in names[1:]:
+            if name[-1:] == '/':
+                os.makedirs(os.path.join(version_output_dir, name[start:]))
+            else:
+                with open(os.path.join(version_output_dir, name[start:]), "wb") as outfile:
+                    outfile.write(version_zipfile.read(name))
 
+    def _add_information_from_download_to_output(self, version):
+        version_output_dir = os.path.join(self.output_directory, version.id, version.version)
+        with open(os.path.join(version_output_dir, "extension.json")) as infile:
+            extension_json = json.load(infile)
+            self.out['extensions'][version.id]['versions'][version.version]['name'] = \
+                extension_json['name']
+            self.out['extensions'][version.id]['versions'][version.version]['description'] = \
+                extension_json['description']
+
+    def _write_output(self):
         with open(os.path.join(self.output_directory, "data.json"), "w") as outfile:
-            json.dump(out, outfile, indent=4)
+            json.dump(self.out, outfile, indent=4)
