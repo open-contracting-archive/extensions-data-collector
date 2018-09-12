a07cfb0c8a6919961d45d23c2c1a8ba5f65b0299
rhiaro <amy@rhiaro.co.uk>
2018-09-12 20:25:39 +0300

    Init babel extractors

---
+++ ocdsextensionsdatacollector/babel_extractors.py
@@ -0,0 +1,8 @@
+from ocdsdocumentationsupport.babel_extractors import extract_codelist, extract_schema
+
+
+def extract_extension_meta(fileobj, keywords, comment_tags, options):
+    pass
+
+def extract_markdown(fileobj, keywords, comment_tags, options):
+    pass
