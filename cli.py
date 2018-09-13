5c81be540636d79754bb8407fc44527496a3f3d2
James Baster <james.baster@opendataservices.coop>
2018-09-13 11:05:07 +0100

    Expand README and add cli.py option

--- cli.py
+++ cli.py
@@ -7,10 +7,11 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--sample", help="Run sample only", action="store_true")
+    parser.add_argument("--outputdir", help="Output Directory. Will be created if not exist.")
 
     args = parser.parse_args()
 
-    runner = ocdsextensionsdatacollector.runner.Runner(sample=args.sample)
+    runner = ocdsextensionsdatacollector.runner.Runner(sample=args.sample, output_directory=args.outputdir)
 
     runner.run()
 
