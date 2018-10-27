a02a480e8c97bf3b9f1bb15692552c2f76d1d5b3
James McKinney <james@slashpoundbang.com>
2018-10-27 11:31:57 -0400

    Fix output directory CLI argument name

--- ocdsextensionsdatacollector/cli/__main__.py
+++ ocdsextensionsdatacollector/cli/__main__.py
@@ -5,13 +5,13 @@ from ocdsextensionsdatacollector.runner import Runner
 
 def main():
     parser = argparse.ArgumentParser(description='OCDS Extensions Data Collector CLI')
-    parser.add_argument('output-dir', help='the directory in which to write the output')
+    parser.add_argument('output_directory', help='the directory in which to write the output')
     parser.add_argument('--limit', help='limit data collection to this many extensions', type=int)
     parser.add_argument('--tx-api-key', help='your Transifex API key')
 
     args = parser.parse_args()
 
-    runner = Runner(args.output_dir, limit=args.limit, tx_api_key=args.tx_api_key)
+    runner = Runner(args.output_directory, limit=args.limit, tx_api_key=args.tx_api_key)
     runner.run()
 
 
