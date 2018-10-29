3f73c997c90439f81265dc359eae66fe744c9473
James McKinney <james@slashpoundbang.com>
2018-10-29 09:31:54 -0400

    Remove normalization test, as this is added to (and tested in) ocdsextensionregistry

--- ocdsextensionsdatacollector/tests.py
+++ ocdsextensionsdatacollector/tests.py
@@ -48,34 +48,6 @@ extension_one,2017-05-09,master,http://example.com/,http://example.com/"""
         'The First Extension'
 
 
-def test_add_information_from_download_to_output_normalise_name_and_description():
-    runner = ocdsextensionsdatacollector.runner.Runner(
-        output_directory=os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'tests',
-            'test_add_information_from_download_to_output_normalise_name_and_description'
-        ),
-    )
-    runner.out = {'extensions': {}}
-
-    extensions_data = """Id,Category,Core
-extension_one,Test,true"""
-
-    extension_versions_data = """Id,Date,Version,Base URL,Download URL
-extension_one,2017-05-09,master,http://example.com/,http://example.com/"""
-
-    registry = ocdsextensionregistry.ExtensionRegistry(extension_versions_data, extensions_data)
-
-    for version in registry:
-        runner._add_registry_metadata_to_output(version)
-        runner._add_information_from_download_to_output(version)
-
-    assert runner.out['extensions']['extension_one']['versions']['master']['name']['en'] == \
-        'Extension One'
-    assert runner.out['extensions']['extension_one']['versions']['master']['description']['en'] == \
-        'The First Extension'
-
-
 def test_add_information_from_latest_version_to_extension():
     with tempfile.TemporaryDirectory() as tmpdir:
         runner = ocdsextensionsdatacollector.runner.Runner(output_directory=tmpdir)
