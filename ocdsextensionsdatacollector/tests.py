f0a0fff6549206e7b4894742d8a04c99269eff46
James McKinney <james@slashpoundbang.com>
2018-10-29 12:29:42 -0400

    Remove test for former methods

--- ocdsextensionsdatacollector/tests.py
+++ ocdsextensionsdatacollector/tests.py
@@ -20,34 +20,6 @@ def test_write_output():
             assert test_out['test_output_to_write'] == 'is_very_good'
 
 
-def test_add_information_from_download_to_output():
-    runner = ocdsextensionsdatacollector.runner.Runner(
-        output_directory=os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'tests',
-            'test_add_information_from_download_to_output'
-        ),
-    )
-    runner.out = {'extensions': {}}
-
-    extensions_data = """Id,Category,Core
-extension_one,Test,true"""
-
-    extension_versions_data = """Id,Date,Version,Base URL,Download URL
-extension_one,2017-05-09,master,http://example.com/,http://example.com/"""
-
-    registry = ocdsextensionregistry.ExtensionRegistry(extension_versions_data, extensions_data)
-
-    for version in registry:
-        runner._add_registry_metadata_to_output(version)
-        runner._add_information_from_download_to_output(version)
-
-    assert runner.out['extensions']['extension_one']['versions']['master']['name']['en'] == \
-        'Extension One'
-    assert runner.out['extensions']['extension_one']['versions']['master']['description']['en'] == \
-        'The First Extension'
-
-
 def test_add_information_from_latest_version_to_extension():
     with tempfile.TemporaryDirectory() as tmpdir:
         runner = ocdsextensionsdatacollector.runner.Runner(output_directory=tmpdir)
