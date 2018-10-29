354ca3679b7f1d921bd3c4877065a9e6033ac5b0
James McKinney <james@slashpoundbang.com>
2018-10-29 09:29:45 -0400

    Use OrderedDict's when generating output to guarantee identical output with identical input

--- ocdsextensionsdatacollector/version_data_collector.py
+++ ocdsextensionsdatacollector/version_data_collector.py
@@ -1,70 +1,58 @@
-import copy
-import csv
-import io
-import json
-import shutil
-import zipfile
-from collections import OrderedDict, defaultdict
-from pathlib import Path
-
-import requests
-
-from ocdsextensionsdatacollector.i18n_helpers import locale_dir
+from collections import OrderedDict
 
 
 class VersionDataCollector:
-    def __init__(self, output_directory, version):
-        self.output_directory = Path(output_directory)
+    def __init__(self, version):
         self.version = version
         self.out = None
 
     def collect(self):
-        self.out = {
-            'date': self.version.date,
-            'base_url': self.version.base_url,
-            'download_url': self.version.download_url,
-            'name': {
+        self.out = OrderedDict([
+            ('date', self.version.date),
+            ('base_url', self.version.base_url),
+            ('download_url', self.version.download_url),
+            ('name', OrderedDict({
                 'en': self.version.metadata['name']['en'],
-            },
-            'description': {
+            })),
+            ('description', OrderedDict({
                 'en': self.version.metadata['description']['en'],
-            },
-            'compatibility': self.version.metadata['compatibility'],
-            'schemas': OrderedDict(),
-            'codelists': OrderedDict(),
-            'docs': OrderedDict(),
-            'readme': {
+            })),
+            ('compatibility', self.version.metadata['compatibility']),
+            ('schemas', OrderedDict()),
+            ('codelists', OrderedDict()),
+            ('docs', OrderedDict()),
+            ('readme', OrderedDict({
                 'en': self.version.remote('README.md'),
-            },
-        }
+            })),
+        ])
 
         for name in ('record-package-schema.json', 'release-package-schema.json', 'release-schema.json'):
             if name in self.version.schemas:
-                self.out['schemas'][name] = {
+                self.out['schemas'][name] = OrderedDict({
                     'en': self.version.schemas[name],
-                }
+                })
             else:
                 self.out['schemas'][name] = {}
 
         for name in sorted(self.version.codelists):
-            self.out['codelists'][name] = {
-                'fieldnames': OrderedDict(),
-                'rows': {},
-            }
+            self.out['codelists'][name] = OrderedDict([
+                ('fieldnames', OrderedDict()),
+                ('rows', OrderedDict()),
+            ])
 
             codelist = self.version.codelists[name]
             for fieldname in codelist.fieldnames:
-                self.out['codelists'][name]['fieldnames'][fieldname] = {
+                self.out['codelists'][name]['fieldnames'][fieldname] = OrderedDict({
                     'en': fieldname,
-                }
+                })
             for row in codelist.rows:
-                self.out['codelists'][name]['rows'][row['Code']] = {
+                self.out['codelists'][name]['rows'][row['Code']] = OrderedDict({
                     'en': OrderedDict(row),
-                }
+                })
 
-        for name, text in self.version.docs.items():
-            self.out['docs'][name] = {
-                'en': text,
-            }
+        for name in sorted(self.version.docs):
+            self.out['docs'][name] = OrderedDict({
+                'en': self.version.docs[name],
+            })
 
         return self.out
